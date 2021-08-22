0af6fe8de83d11c7eccade2deb665a3976cc132d
Abhijith Ashokan <81083505+Abhijith-Ashokan@users.noreply.github.com>
2021-08-22 20:31:53 +0530

    Add files via upload

--- 3_Implementation/adders&subtractors/src/addersub.c
+++ 3_Implementation/adders&subtractors/src/addersub.c
@@ -2,42 +2,49 @@
 #include"header.h"
 
 
-void Full_Adder(int A,int B,int C_In){
-    int Sum , C_Out;
+int Full_Adder_sum(int A,int B,int C_In){
+    int Sum ;
     // Calculating value of sum
-    Sum = C_In ^ (A ^ B);
-    
+   return  Sum = C_In ^ (A ^ B);
+} 
+int Full_Adder_carry(int A,int B,int C_In){
     //Calculating value of C-Out
-    C_Out = (A & B) || (B & C_In) || (A & C_In);
+    int C_Out;
+   return C_Out = (A & B) || (B & C_In) || (A & C_In);
    
-   // printing the values
-    printf("%d = ",Sum);
-    printf("%d = ",C_Out);
   }
 
-  void Full_Subtractor(int A,int B,int Bin){
+  int Full_Subtractor_Dif(int A,int B,int Bin){
    
    // Calculating value of Difference
     int Difference = (A ^ B) ^ Bin;
+    return Difference;
+  }
+  int Full_Subtractor_bout(int A,int B,int Bin){
        
     //calculating NOT  value of a
-    int A1 = not(A);
+    int A1 = !A;
        
     //Calculating value of B-Out
     int B_Out = A1 & Bin | A1 & B | B & Bin;
-   
-    //printing the values
-    printf("Difference = %d",Difference);
-    printf("B-Out = %d",B_Out);
+   return B_Out;
    
   }
 
   bit halfadd(bit A,bit B){
- carry=A&B;
-   return A^B;
+   
+    return A^B;
+ }
+
+ bit halfadd_carry(bit A,bit B){
+   return A&B;
+    
  }
 bit halfsub(bit A,bit B){
- borrow=!A&B;
- return A^B;
-      }
-
+    
+    return A^B;
+}
+bit halfsub_b(bit A,bit B){
+    return !A&B;
+   
+}
