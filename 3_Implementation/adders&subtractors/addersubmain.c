5ec64d5c25329f8adb5f0f066912221f96187bcc
Abhijith Ashokan <81083505+Abhijith-Ashokan@users.noreply.github.com>
2021-08-22 20:32:18 +0530

    Add files via upload

--- 3_Implementation/adders&subtractors/addersubmain.c
+++ 3_Implementation/adders&subtractors/addersubmain.c
@@ -1,48 +1,53 @@
 #include<stdio.h>
+#include "header.h"
 
 int main()
 {
-    int c;
-    printf("Adders and Subtractors:\nEnter your choice:\n1. Half Adder \n2. Full Adder\n 3. Half Subtractor\n 4. Full subtractor\n")
-    scanf("%d.&c");
+    int choice;
+    printf("Adders and Subtractors:\nEnter your choice:\n1. Half Adder \n2. Full Adder\n 3. Half Subtractor\n 4. Full subtractor\n");
+    scanf("%d",&choice);
 
     if(choice ==1)
     {
-        int i,j,result;
+        int i,j,result,carry;
         printf("Input A and b");
         scanf("%d%d",&i,&j);
         
         result=halfadd(i,j);
-        printf("%d   %d  |  ",i,j);
-        printf("%d   %d\n",result,carry);
+        carry = halfadd_carry(i,j);
+        
+        printf("result = %d carry= %d\n",result,carry);
             
 
     }
     else if (choice==2)
     {
-       int i,j,k;
+       int i,j,k,sum,cout;
        printf("Input A, b and Cin");
        scanf("%d%d%d",&i,&j,&k);
-        Full_Adder(i, j, k);
-
+       sum= Full_Adder_sum(i, j, k);
+        cout=Full_Adder_carry(i,j,k);
+        printf("Sum = %d, Cout = %d",sum,cout);
     }
     else if(choice ==3)
     {
-         int i,j,result;
+         int i,j,result,borrow;
         printf("Input A and b");
         scanf("%d%d",&i,&j);
         result=halfsub(i,j);
-        printf("%d   %d  |   ",i,j);
-        printf("%d    %d\n",result,borrow);
+        borrow = halfsub_b(i,j);
+        printf("result = %d borrow = %d\n",result,borrow);
 
     }
     else if(choice == 4)
     {
-        int i,j,k;
+        int i,j,k,dif,bout;
         printf("Input A, b and Cin");
         scanf("%d%d%d",&i,&j,&k);
-        Full_Subtractor(i, j, k);
-}
+        dif =Full_Subtractor_Dif(i, j, k);
+        bout = Full_Subtractor_bout(i,j,k);
+        printf("Difference = %d B_Out = %d",dif,bout);
+
     }
 }
 
