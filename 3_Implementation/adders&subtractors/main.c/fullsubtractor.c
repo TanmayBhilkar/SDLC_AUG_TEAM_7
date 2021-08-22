cf1465149a26002840f79f7c7611d965152bb716
Prakruthi-BR <82602869+Prakruthi-BR@users.noreply.github.com>
2021-08-22 11:41:47 +0530

    Create fullsubtractor.c

---
+++ 3_Implementation/adders&subtractors/main.c/fullsubtractor.c
@@ -0,0 +1,12 @@
+int main() {
+    int i,j,k;
+    for(i=0;i<2;i++){
+	for(j=0;j<2;j++){
+		for(k=0;k<2;k++){
+    // passing three inputs of fullsubtractor as arguments to get result function
+    Full_Subtractor(i, j, k);
+}
+}
+}
+    return 0;
+}
