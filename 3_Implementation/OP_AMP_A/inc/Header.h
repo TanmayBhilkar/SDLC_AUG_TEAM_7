0ee76d2757245f0845ac837cf0f65bfadb274514
TanmayBhilkar <86276947+TanmayBhilkar@users.noreply.github.com>
2021-08-22 10:24:02 +0530

    Add files via upload

---
+++ 3_Implementation/OP_AMP_A/inc/Header.h
@@ -0,0 +1,9 @@
+#ifndef _HEADER_H_
+#define _HEADER_H_
+
+int Inv(double Rf,double Rin,double Vin);
+int Diff(double Rf,double Rin,double Vin,double V1);
+int Non_inv(double Rf,double Rin,double Vin);
+int summ(double Rf,double Rin,double Vin,double V1);
+
+#endif
