e3dbde28d0b7ff9d85c66f80655294f42b2120ec
TanmayBhilkar <86276947+TanmayBhilkar@users.noreply.github.com>
2021-08-22 10:23:00 +0530

    Delete 3_Implementation/Op_Amp directory

--- 3_Implementation/Op_Amp/inc/Header.h
+++
@@ -1,9 +0,0 @@
-#ifndef _HEADER_H_
-#define _HEADER_H_
-
-int Inv(double Rf,double Rin,double Vin);
-int Diff(double Rf,double Rin,double Vin,double V1);
-int Non_inv(double Rf,double Rin,double Vin);
-int summ(double Rf,double Rin,double Vin,double V1);
-
-#endif
