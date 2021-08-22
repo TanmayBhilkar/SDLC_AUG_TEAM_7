d318a966a124dcafc3672c6a9b27b4a07b38cd05
Abhijith Ashokan <81083505+Abhijith-Ashokan@users.noreply.github.com>
2021-08-22 20:31:14 +0530

    Add files via upload

--- 3_Implementation/adders&subtractors/inc/header.h
+++ 3_Implementation/adders&subtractors/inc/header.h
@@ -2,16 +2,20 @@
 #define __HEADER_H__
 //halfadder
 typedef char bit;
- bit carry=0;
+// bit carry=0;
 //halfsubtractor
 typedef char bit;
-   bit borrow=0;
+  // bit borrow=0;
 
-void Full_Adder(int,int,int);
-void Full_Subtractor(int,int,int);
+int Full_Adder_sum(int,int,int);
+int Full_Adder_carry(int,int,int);
+int Full_Subtractor_Dif(int,int,int);
+int Full_Subtractor_bout(int,int,int);
  bit halfadd(bit,bit);
+ bit halfadd_carry(bit,bit);
 
 bit halfsub(bit,bit);
+bit halfsub_b(bit,bit);
 
 
 #endif
