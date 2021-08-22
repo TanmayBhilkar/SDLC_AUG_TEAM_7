55c194a9d053770829fe30bc4f438664bcfc4d92
Abhijith Ashokan <81083505+Abhijith-Ashokan@users.noreply.github.com>
2021-08-22 19:12:54 +0530

    Add files via upload

--- 3_Implementation/adders&subtractors/inc/header.h
+++ 3_Implementation/adders&subtractors/inc/header.h
@@ -1,8 +1,17 @@
-#include <stdio.h>
- #include <conio.h>
+#ifndef __HEADER_H__
+#define __HEADER_H__
 //halfadder
 typedef char bit;
  bit carry=0;
 //halfsubtractor
 typedef char bit;
    bit borrow=0;
+
+void Full_Adder(int,int,int);
+void Full_Subtractor(int,int,int);
+ bit halfadd(bit,bit);
+
+bit halfsub(bit,bit);
+
+
+#endif
