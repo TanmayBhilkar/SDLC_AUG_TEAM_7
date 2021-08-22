0dc14f2f422f2aa122cea96a258c69e8185361a7
Prakruthi-BR <82602869+Prakruthi-BR@users.noreply.github.com>
2021-08-22 11:43:01 +0530

    Create halfadder.c

---
+++ 3_Implementation/adders&subtractors/main.c/halfadder.c
@@ -0,0 +1,15 @@
+int main()
+{
+ int i,j,result;
+ printf("A   B  |  S  Carry\n");
+ for(i=0;i<2;i++)
+ {
+  for(j=0;j<2;j++)
+  {
+   result=halfadd(i,j);
+   printf("%d   %d  |  ",i,j);
+   printf("%d   %d\n",result,carry);
+  }
+ }
+ return 0;
+}
