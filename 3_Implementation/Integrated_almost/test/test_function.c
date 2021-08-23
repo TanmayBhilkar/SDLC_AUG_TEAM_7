15cb023fe32faf6ea37e925bd934c5a814b09129
Jayashree.S <89181463+jayu-shree@users.noreply.github.com>
2021-08-23 21:11:54 +0530

    Update test_function.c

--- 3_Implementation/Integrated_almost/test/test_function.c
+++ 3_Implementation/Integrated_almost/test/test_function.c
@@ -278,24 +278,28 @@ void test_srflipflop(void)
 {
 	TEST_ASSERT_EQUAL(1,srflipflop(1,0,0));
 	TEST_ASSERT_EQUAL(0,srflipflop(0,1,1));
+	TEST_ASSERT_EQUAL(-1,srflipflop(1,2,1));
 }
 
 void test_jkflipflop(void)
 {
 	TEST_ASSERT_EQUAL(0,jkflipflop(0,0,0));
 	TEST_ASSERT_EQUAL(1,jkflipflop(1,0,1));
+	TEST_ASSERT_EQUAL(-1,jkflipflop(2,2,1));
 }
 
 void test_dflipflop(void)
 {
 	TEST_ASSERT_EQUAL(1,dflipflop(1));
 	TEST_ASSERT_EQUAL(0,dflipflop(0));
+	TEST_ASSERT_EQUAL(-1,dflipflop(5));
 }
 
 void test_tflipflop(void)
 {
 	TEST_ASSERT_EQUAL(1,tflipflop(1,0));
 	TEST_ASSERT_EQUAL(1,tflipflop(0,1));
+	TEST_ASSERT_EQUAL(-1,tflipflop(6,7));
 }
 
 //----------------LOGIC GATES----------------------------------------
